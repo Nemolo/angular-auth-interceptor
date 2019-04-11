42a61aac7672873cdc95a08ad20dfe8ed6d746aa
Fabrizio Marconi <tdlemon@gmail.com>
2019-04-11 10:52:31 +0200

    failing to create provider

--- projects/auth-header-interceptor/src/lib/auth-header-interceptor.interceptor.ts
+++ projects/auth-header-interceptor/src/lib/auth-header-interceptor.interceptor.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { switchMap, catchError, share } from 'rxjs/operators';
+import { AuthHeaderInterceptorConfig } from './auth-header-interceptor.config';
 
 @Injectable({
   providedIn: 'root'
@@ -11,36 +12,29 @@ export class AuthHeaderInterceptorService implements HttpInterceptor {
   authRequest: Observable<any>;
 
   constructor(
-    private whiteList: Set<string>,
-    private failCallback: CallableFunction,
-    private tokenValidatorFunction: CallableFunction,
-    private refreshValidatorFunction: CallableFunction,
-    private logoutFunction: CallableFunction,
-    private removeTokenFunction: CallableFunction,
-    private refreshTokenFunction: CallableFunction,
-    private getTokenFunction: CallableFunction,
+    private config: AuthHeaderInterceptorConfig
     ) { }
 
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     // add authorization header with jwt token if available
     if (this.needsAuth(request.url)) {
-      if (this.tokenValidatorFunction()) {
+      if (this.config.tokenValidatorFunction()) {
         return next.handle(this.getClonedRequest(request))
           .pipe(
             catchError(err => {
               if (err.status === 401) {
-                  this.removeTokenFunction();
+                  this.config.removeTokenFunction();
                   return this.refreshCall(request, next);
               } else {
                   return throwError(err);
               }
             })
           );
-      } else if (this.refreshValidatorFunction()) {
+      } else if (this.config.refreshValidatorFunction()) {
         return this.refreshCall(request, next);
       } else {
-        this.logoutFunction();
+        this.config.logoutFunction();
         return throwError('Unauthorized');
       }
     }
@@ -49,10 +43,10 @@ export class AuthHeaderInterceptorService implements HttpInterceptor {
 
   protected refreshCall(request: HttpRequest<any>, next: HttpHandler): Observable<any> {
     if (!this.authRequest) {
-      this.authRequest = this.refreshTokenFunction().pipe(
+      this.authRequest = this.config.refreshTokenFunction().pipe(
         catchError((err, caught) => {
           if (err.status === 401) {
-            this.logoutFunction();
+            this.config.logoutFunction();
             return next.handle(request);
           } else {
             return throwError(err);
@@ -70,7 +64,7 @@ export class AuthHeaderInterceptorService implements HttpInterceptor {
 
   needsAuth(url) {
     // no auth
-    return !this.whiteList.has(url);
+    return !this.config.whiteList.has(url);
     // if (url.startsWith(environment.endpoint)) {
     //   for (const oneUrl of this.whiteList) {
     //     if ((environment.endpoint + oneUrl) === url) {
@@ -85,7 +79,7 @@ export class AuthHeaderInterceptorService implements HttpInterceptor {
   protected getClonedRequest(request: HttpRequest<any>) {
     return request.clone({
       setHeaders: {
-        Authorization: `Bearer ${this.getTokenFunction()}`
+        Authorization: `Bearer ${this.config.getTokenFunction()}`
       }
     });
   }
--- projects/auth-header-interceptor/src/lib/auth-header-interceptor.module.ts
+++ projects/auth-header-interceptor/src/lib/auth-header-interceptor.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { AuthHeaderInterceptorService } from './auth-header-interceptor.interceptor';
+import { AUTH_HEADER_INTERCEPTOR_CONFIG, AUTH_HEADER_INTERCEPTOR_CONFIG_FACTORY } from './auth-header-interceptor.config';
 
 @NgModule({
   declarations: [],
   imports: [],
+  providers: [
+    AUTH_HEADER_INTERCEPTOR_CONFIG_FACTORY
+  ],
   exports: [ AuthHeaderInterceptorService ]
 })
 export class AuthHeaderInterceptorModule { }
